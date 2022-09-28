de6052bad88aba8f98d276f2ad47bf99b70e3d48
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-09-28 11:41:53 +0900

    Update 문자열을 정수로 바꾸기.kt

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.kt
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.kt
@@ -2,7 +2,9 @@ class Solution {
     fun solution(s: String): Int {
         var answer = s.toInt()
         
+//         var list = s.toCharArray().joinToString("").toInt()
 
+//         if(s.toCharArray().joinToString("").toInt()>0) answer =list else answer = list
 
         return answer
     }
