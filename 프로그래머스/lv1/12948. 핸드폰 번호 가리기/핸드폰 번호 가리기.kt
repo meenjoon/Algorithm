ff6fa4446f9b140554e26cb61082fd9eac132149
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-09-29 23:21:15 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 10.92 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -0,0 +1,4 @@
+class Solution {
+    fun solution(phone_number: String): String =
+        "${"*".repeat(phone_number.length - 4)}${phone_number.substring(phone_number.length - 4, phone_number.length)}"
+}
