405e0017db8e27b47da2e8b4b7daadbe1948ee6d
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-10-05 23:20:13 +0900

    [level 1] Title: 내적, Time: 0.06 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.kt
+++ 프로그래머스/lv1/70128. 내적/내적.kt
@@ -3,18 +3,12 @@ class Solution {
         var answer: Int = 1234567890
         var array = arrayOfNulls<Int>(a.size)
         var aa = 0
-        
-        
+
+
         for((indexa, elementa) in a.withIndex()) {
-            array[indexa] = a[indexa] * b[indexa]
+            aa += a[indexa] * b[indexa]
         }
-        
-        for (temp in array){
-            if (temp != null) {
-                aa += temp
-            }
-        }
-        
+
         return aa
     }
 }
