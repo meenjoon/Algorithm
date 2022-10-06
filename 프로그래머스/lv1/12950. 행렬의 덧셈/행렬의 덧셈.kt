fdae9908d1a309d1d9c621dd9bd829ac45e14b3f
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-10-06 22:16:08 +0900

    [level 1] Title: 행렬의 덧셈, Time: 5.59 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.kt
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.kt
@@ -1,7 +1,11 @@
 class Solution {
     fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-        var answer = arrayOf<IntArray>()
-        answer = arr1.mapIndexed { index, arr -> arr.mapIndexed { index2, value -> arr2[index][index2] + value  }.toIntArray()   }.toTypedArray()
+        var answer = Array(arr1.size){IntArray(arr1[0].size)}
+        for((index1, element2) in arr1.withIndex()){
+            for((index2, element2) in element2.withIndex()){
+                answer[index1][index2] = element2 + arr2[index1][index2]
+            }
+        }
         return answer
     }
 }
