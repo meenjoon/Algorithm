8223efc0d37b22c473a2a4b3049be00afb5f5703
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-10-08 02:19:18 +0900

    [level 1] Title: 모의고사, Time: 28.70 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.kt
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.kt
@@ -1,22 +1,20 @@
 class Solution {
     fun solution(answers: IntArray): IntArray {
         var answer = intArrayOf()
-        var array = intArrayOf()
+        
         var list = mutableListOf<Int>()
         var list2 = mutableListOf<Int>()
+       
         var max = 0
-
-
-
+        
         var ee = answers
 
         var array2 = intArrayOf(1,2,3,4,5)
         var array3 = intArrayOf(2,1,2,3,2,4,2,5)
         var array4 = intArrayOf(3,3,1,1,2,2,4,4,5,5)
+        
         var aa = answers.size -1
-
-        var ss = aa/5
-
+        
         var count1 = 0
         var count2 = 0
         var count3 =0
@@ -25,21 +23,6 @@ class Solution {
             ee[index] = element
         }
 
-//        if(ss>1){
-//            for((index,element) in (1..ss).withIndex())
-//                array = answer.slice(0..4).toIntArray()
-//        }
-//        for((index,element) in array.withIndex() ){
-//            if(array[index] == array2[index]){
-//                count1++
-//            }
-//            if(array[index] == array3[index]){
-//                count2++
-//            }
-//            if(array[index] == array4[index]){
-//                count3++
-//            }
-//        }
         for(index in (0..aa) ){
 
             if(ee[index] == array2[index % array2.size]){
@@ -69,18 +52,10 @@ class Solution {
         if(max == count3){
             list2.add(3)
         }
-        println(list2)
 
-
-
+        
         answer = list2.toIntArray()
-        println("$count1 $count2 $count3")
 
-//        if(count1 == count2&& count1 == count3 && count2 == count3) {
-//            return intArrayOf(1,2,3)
-//        }
-//        if(count1>)
-
-        return answer
+        return list2.toIntArray()
     }
 }
