cda518ccf154f72d045553123130648c77cbe038
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-10-08 15:15:33 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.kt
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.kt
@@ -1,18 +1,32 @@
 class Solution {
     fun solution(nums: IntArray): Int {
-        
-        var answer = 0
-        for (i in 0 until nums.size - 2) {
-            for (j in i + 1 until nums.size - 1) {
-                for (k in j + 1 until nums.size) {
-                    if (isPrime(nums[i] + nums[j] + nums[k])) answer++
+        var answer = -1
+        var list = mutableListOf<Int>()
+        var count = 0
+
+        for (i in 0..nums.size - 3) {
+            for (j in i + 1..nums.size - 2) {
+                for (k in j + 1..nums.size - 1) {
+                    list.add(nums[i] + nums[j] + nums[k])
+                    if(isPrime(nums[i] + nums[j] + nums[k]) == true){
+                        count++
+                    }
                 }
             }
         }
+
+        answer = count
+
         return answer
-        
     }
-    
-    fun isPrime(n: Int): Boolean = n > 1 && (2 until n).none { n % it == 0 }
-    
+
+        fun isPrime(n: Int): Boolean {
+            var i = 2
+            while(i * i <= n){
+                if(n % i++ == 0) {
+                    return false
+                }
+            }
+            return true
+        }
 }
