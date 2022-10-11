559ae100b50d4fa172b020388b7756f999603d11
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-10-11 16:12:52 +0900

    [level 1] Title: 체육복, Time: 29.94 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.kt
+++ 프로그래머스/lv1/42862. 체육복/체육복.kt
@@ -1,44 +1,17 @@
 class Solution {
     fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
-        var arr = Array(n+1){1}
-        var answer = -1
+        var answer = n
 
-        // 체육복 도난
-        for(i in lost){
-            arr[i]--
-        } 
+        val aa = lost.sorted().toSet()-reserve.sorted().toSet()
+        val bb = (reserve.sorted().toSet()-lost.sorted().toSet()).toMutableSet()
 
-        // 여벌의 체육복
-        for(i in reserve){
-            arr[i]++
-        } 
-
-        for(i in 1..n){
-            // 체육복이 도난 당했다면
-            if(arr[i]==0){
-                
-                // 왼쪽 친구가 있으면 빌림
-                if(arr[i-1] == 2){
-                    arr[i-1]--
-                    arr[i]++
-                } 
-                
-                // 오른쪽 친구가 있으면 빌림
-                else if(i<n){
-                    if(arr[i+1] == 2){
-                        arr[i+1]--
-                        arr[i]++
-                    }
-                }
+        for(temp in aa){
+            when {
+                temp - 1 in bb -> bb.remove(temp - 1)
+                temp + 1 in bb -> bb.remove(temp + 1)
+                else -> answer--
             }
         }
-
-        // 체육복을 2개가지고 있는 사람은 하나로 처리
-        for(i in arr){
-            answer += if (i == 2) 1
-            else i
-        }
-
         return answer
     }
 }
