ad24ef18681906d6de76d30000c3b303670d867f
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-10-12 22:13:11 +0900

    [Bronze V] Title: 시험 성적, Time: 92 ms, Memory: 12344 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+    val A = readln().toInt()
+
+
+     val B = when(A) {
+        in 90 .. 100 -> "A"
+        in 80 .. 89 -> "B"
+        in 70 .. 79 -> "C" 
+        in 60 .. 69 -> "D"
+        else -> "F"
+     }
+    println(B)
+
+}
