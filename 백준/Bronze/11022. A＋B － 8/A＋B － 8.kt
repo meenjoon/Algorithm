c543393bc34eb6daff78c110b0abb9a20d099225
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-10-13 14:54:19 +0900

    [Bronze V] Title: A+B - 8, Time: 148 ms, Memory: 20636 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var A = readln().toInt()
+
+    for(temp in 1..A){
+        var (B,C) = readln().split(" ").map { it.toInt() }
+        println("Case #$temp: $B + $C = ${B+C}")
+    }
+}
