371170a8a919c67119e4414beb426bd672c2adee
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-11-02 17:22:23 +0900

    [Silver V] Title: 나이순 정렬, Time: 632 ms, Memory: 56956 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.kt
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.kt
@@ -1,16 +1,35 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import java.util.*
+import kotlin.collections.ArrayList
 
-fun main(args:Array<String>) = with(Scanner(System.`in`)){
-    var n = nextInt()
-    var list= arrayListOf<Pair<Int,String>>()
+fun main() {
 
-    for(i in 0 until n){
-        list.add(Pair(nextInt(),nextLine()))
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val st = StringTokenizer(br.readLine())
+    val n = st.nextToken().toInt()
+
+    var arraylist = ArrayList<Pair<Int,String>>()
+    
+    for(temp in 1..n){
+        val st2 = StringTokenizer(br.readLine())
+        var a = st2.nextToken().toInt()
+        var b = st2.nextToken().toString()
+
+        arraylist.add(Pair(a,b))
     }
 
-    var sortList = list.sortedWith(compareBy({it.first}))
+    arraylist.sortWith(compareBy({it.first}))
 
-    for(i in 0 until n) {
-        println("${sortList[i].first}${sortList[i].second}")
+    for((index,temp) in (1..n).withIndex()) {
+        bw.write("${arraylist[index].first} ${arraylist[index].second}\n")
     }
+
+    bw.flush()
+    bw.close()
+
 }
