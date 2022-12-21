5ff93669426f87e8fa3658f22d99c507d669ce74
meenjoon <88024665+meenjoon@users.noreply.github.com>
2022-12-21 14:39:39 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 92 ms, Memory: 12240 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.kt
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.kt
@@ -2,40 +2,27 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.lang.reflect.Array
 import java.util.*
-import kotlin.collections.ArrayList
+
 
 fun main() {
-
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
     val st = StringTokenizer(br.readLine())
     val n = st.nextToken().toInt()
 
-    var fib = 0
-
-    val array = IntArray(n+2)
-
-    array[0] = 0
-    array[1] = 1
-
-
-    if(n>=3) {
-        for (temp in 1..n - 1) {
-            array[temp + 1] = array[temp - 1] + array[temp]
-            fib = array[temp + 1]
+    fun fib (a : Int): Int {
+        if(a == 0) {
+            return 0
         }
-    }
-    else if(n==2 || n== 1){
-        fib = 1
-    }
-    else if (n==0){
-        fib = 0
-    }
+        if(a == 1) {
+            return 1
+        }
+        return fib(a-2) + fib(a-1)
 
-    bw.write("$fib")
+    }
+    bw.write("${fib(n)}")
     bw.flush()
     bw.close()
 }
