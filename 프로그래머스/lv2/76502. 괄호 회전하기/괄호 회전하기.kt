dac0107a31540a2af0641af5ad9d3c0c088ab8ce
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-02-09 14:36:50 +0900

    [level 2] Title: 괄호 회전하기, Time: 9.64 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.kt
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.kt
@@ -1,31 +1,65 @@
-import java.util.Stack
 
 class Solution {
     fun solution(s: String): Int {
-        // s를 한칸씩 회전하면서 isTrue의 조건을 만족하는 개수를 반환
-        return s.indices.count{ isTrue(s.substring(it, s.length) + s.substring(0, it)) }
-    }
+        var answer: Int = -1
 
-    fun isTrue(s:String):Boolean{
-        val openBracket = charArrayOf('(', '{', '[')
-        val closeBracket = charArrayOf(')', '}', ']')
-        val stack = Stack<Char>()
-        var flag = false
-        // 만일 닫힌 괄호로 시작한다면 false반환
-        for(c in closeBracket) if(c == s[0]) return false
 
-        for(c in s){
-            flag = false
-            for(i in openBracket.indices)
-                // 스텍이 비어있지 않고 직전에 넣은 스텍이 열린 괄호 이면서 닫는 괄호와 짝이 맞는 경우 flag를 true로 변경
-                if(stack.isNotEmpty() && stack.peek() == openBracket[i] && c == closeBracket[i])
-                    flag = true
-            // flag가 true이면 stack pop
-            if(flag) stack.pop()
-            // flag가 false 이면 stack에 c추가
-            else stack.add(c)
+        val set = mutableSetOf<Char>()
+
+        val stringBuilder = StringBuilder()
+
+        var totalCount = 0
+
+        var boolean = true
+
+
+        for(index in 0 until s.length) {
+            val a = s.substring(index, s.length) + s.substring(0,index)
+//            for(index2 in index until s.length) {
+//                    set.add(s[index2])
+//            }
+//
+//            for(index3 in 0..index) {
+//                set.add(s[index3])
+//            }
+//
+//            val list = set.toList()
+
+
+            for(index4 in 0 until a.length) {
+
+                if(stringBuilder.isEmpty()) {
+                    stringBuilder.append(a[index4])
+                }
+                else if(a[index4] == ')' && stringBuilder.last() =='(') {
+                    stringBuilder.deleteCharAt(stringBuilder.length-1)
+                }
+                else if(a[index4] == '}' && stringBuilder.last() =='{') {
+                    stringBuilder.deleteCharAt(stringBuilder.length-1)
+                }
+                else if(a[index4] == ']' && stringBuilder.last() =='[') {
+                    stringBuilder.deleteCharAt(stringBuilder.length-1)
+                }
+                else {
+                    stringBuilder.append(a[index4])
+                }
+            }
+//            println("$index $stringBuilder")
+
+            if(stringBuilder.isEmpty()) {
+//                println("index : $index")
+                totalCount++
+            }
+
+//            println(set)
+//            set.clear()
+            stringBuilder.clear()
         }
-        // 스택이 비어있으면 모두 올바른 괄호
-        return stack.isEmpty()
+
+//        println(totalCount)
+
+        answer = totalCount
+
+        return answer
     }
 }
