2d4f2aa76da7631b3434c9eaf25882a74507cca4
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-02-10 13:55:00 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.84 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+        var answer = Array(arr1.size){IntArray(arr2[0].size)}
+
+        for(i in 0 until arr1.size) {
+            for(j in 0 until arr2[0].size) {
+                for(k in 0 until arr1[0].size) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j]
+                }
+            }
+        }
+//        answer.forEach{ it.forEach{ print(it) } }
+
+        return answer
+    }
+}
