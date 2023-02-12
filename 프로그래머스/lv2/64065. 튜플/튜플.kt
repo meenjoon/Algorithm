ebbe31a011619bef282608b5a4e20c402409c2bd
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-02-12 23:08:54 +0900

    [level 2] Title: 튜플, Time: 12.63 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.kt
+++ 프로그래머스/lv2/64065. 튜플/튜플.kt
@@ -4,10 +4,11 @@ class Solution {
 
         val list = mutableListOf<Int>()
 
-        val s_Custum = s.substring(2,s.length-2).split("},{").sortedBy { it.length }
-            .forEach { it.split(",").forEach { if(!list.contains(it.toInt())) {
-            list.add(it.toInt())}
-            } }
+        s.substring(2,s.length-2).split("},{").sortedBy { it.length }
+            .forEach { it.split(",")
+                .forEach { if(!list.contains(it.toInt())) {
+                    list.add(it.toInt())}} 
+            }
 //        list.forEach { print(it) }
         
         answer = list.toIntArray()
