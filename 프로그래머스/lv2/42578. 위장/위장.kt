1296a287745c523fc2571dda10617d4b6e1bd5d4
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-02-13 12:29:09 +0900

    [level 2] Title: 위장, Time: 0.22 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.kt
+++ 프로그래머스/lv2/42578. 위장/위장.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int {
-        var answer = 0
+        var answer = 1
         val map = mutableMapOf<String, Int>()
 
         for(index in 0 until clothes.size) {
@@ -12,11 +12,15 @@ class Solution {
             }
         }
 
-        // println(map)
+        for(keyinValue in map) {
+            answer *= keyinValue.value+1
+        }
 
-        answer = map.values.fold(1) { total, num -> total * (num+1)} // 중요한건 초기값 1부터 시작해서 total의 값이 1인것이다. 그 후 계속 연산을 해서 그 결과값은 total의 값은 중첩된다.
+        /*
+        fold() 함수 이용, 초기값이 중요하다. 그 후, 첫번째 인자는 계산된 값이 계속 들어가고, 2번째 인자는 계산될 Value가 계속 들어간다.
+         */
+//        answer = map.values.fold(1) { total, num -> total * (num+1)} // 중요한건 초기값 1부터 시작해서 total의 값이 1인것이다. 그 후 계속 연산을 해서 그 결과값은 total의 값은 중첩된다.
 
-        // println(answer)
         return answer -1
     }
 }
