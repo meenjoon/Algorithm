0c94a0c5f4d3183d52ab16bb3463fa34121a9807
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-02-19 23:11:16 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 607.53 ms, Memory: 503 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(elements: IntArray): Int {
+
+        val newArr = IntArray(elements.size * 2) { 0 }
+
+        for (i in newArr.indices) {
+            newArr[i] = elements[i % elements.size]
+        }
+
+        val set = mutableSetOf<Int>()
+
+        for (i in 1..elements.size) {
+            for (j in 0..elements.size) {
+                set.add(newArr.copyOfRange(j, j+i).sum())
+            }
+        }
+
+        return set.size
+    }
+}
