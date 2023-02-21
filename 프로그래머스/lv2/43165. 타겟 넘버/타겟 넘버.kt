06a840e866ffd5e96acaf70256dde5eaa37f5874
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-02-21 14:23:56 +0900

    [level 2] Title: 타겟 넘버, Time: 0.66 ms, Memory: 58.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun solution(numbers: IntArray, target: Int): Int {
+        var answer = 0
+
+        fun dfs(index:Int, sum: Int) {
+
+            if(index == numbers.size) {
+                if(sum == target) {
+                    answer++
+                }
+            } 
+            else {
+                dfs(index + 1, sum + numbers[index])
+                dfs(index + 1, sum - numbers[index])
+            }
+        }
+        dfs(0,0)
+
+        return answer
+    }
+}
