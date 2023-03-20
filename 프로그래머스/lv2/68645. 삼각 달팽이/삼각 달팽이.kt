4c12e5e22b4cbc920ee517fdc26910f3b49724da
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-03-20 11:35:37 +0900

    [level 2] Title: 삼각 달팽이, Time: 17.93 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.kt
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.kt
@@ -1,6 +1,47 @@
+//참고 : https://minhamina.tistory.com/58
 class Solution {
     fun solution(n: Int): IntArray {
-        var answer: IntArray = intArrayOf()
+        var answer = IntArray(n*(n+1)/2)
+
+        val matrix = Array(n) {IntArray(n)}
+
+
+
+        var x = -1
+        var y = 0
+
+        var num = 1
+
+        for(index in 0 until n) {
+            for(index2 in index until n) {
+//                println("[$index][$index2] ")
+                if (index % 3 == 0) {
+                    x++
+                } else if(index % 3 == 1) {
+                    y++
+                } else if(index % 3 == 2) {
+                    x--
+                    y--
+                }
+                matrix[x][y] = num++
+            }
+        }
+
+        var k = 0
+
+        for(index in 0 until n) {
+            for(index2 in 0 until n) {
+//                print("[$index][$index2]:${matrix[index][index2]} ")
+                if(matrix[index][index2] == 0) {
+                    break
+                }
+                answer[k++] = matrix[index][index2]
+            }
+        }
+
+//        answer.forEach { print(it) }
+
+
         return answer
     }
 }
