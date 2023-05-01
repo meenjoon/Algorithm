f290e1b79968c7b29f2f28d3cb6c800109ae8a2e
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-05-01 20:49:41 +0900

    [level 2] Title: n^2 배열 자르기, Time: 18.68 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.kt
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.kt
@@ -1,12 +1,21 @@
 class Solution {
     fun solution(n: Int, left: Long, right: Long): IntArray {
+        
         val answer = IntArray((right-left).toInt()+1)
-        
-        for((index,value) in (left..right).withIndex()) {
-            val num = Math.max(value % n, value/ n) + 1
-            answer[index] = num.toInt()
+
+        /* 방법1.
+         */
+//        for((index,value) in (left..right).withIndex()) {
+//            val num = Math.max(value % n, value/ n) + 1
+//            answer[index] = num.toInt()
+//        }
+
+        /* 방법2.
+        */
+        (left..right).mapIndexed { index, value ->
+            val a = Math.max(value % n,value / n) + 1
+            answer[index] = a.toInt()
         }
-        
         return answer
     }
 }
