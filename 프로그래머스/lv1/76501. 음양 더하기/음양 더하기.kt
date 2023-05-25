d5d93e2926148e59e98ee66e8a1ee4250dc60292
meenjoon <88024665+meenjoon@users.noreply.github.com>
2023-05-25 22:48:26 +0900

    [level 1] Title: 음양 더하기, Time: 6.26 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.kt
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.kt
@@ -1,12 +1,12 @@
 class Solution {
     fun solution(absolutes: IntArray, signs: BooleanArray): Int {
+        
         val answer: Int = 123456789
         val array = arrayOfNulls<Int>(absolutes.size)
         var ss: Int = 0
 
         for((temp, tempss) in absolutes.withIndex()) {
             val i: Int = 0
-//            println("aaa $tempss")
 
             if(signs[temp]){
                 array[temp] = tempss
@@ -22,7 +22,6 @@ class Solution {
                 println(ss)
             }
         }
-
 
         return ss
     }
