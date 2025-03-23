719bc0f9804f1845733aaccfd584a92dd2c798f6
meenjoon <88024665+meenjoon@users.noreply.github.com>
2025-03-23 19:29:15 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 100 ms, Memory: 14832 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.kt
@@ -0,0 +1,13 @@
+import java.util.*
+import java.io.*
+import kotlin.math.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
+    while (true) {
+        val line = readLine() ?: break
+        println(line)
+    }
+}
