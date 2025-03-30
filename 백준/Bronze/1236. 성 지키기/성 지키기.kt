c6b345b59565f99e75a474e5476e95a2fadabdb6
meenjoon <88024665+meenjoon@users.noreply.github.com>
2025-03-30 16:47:21 +0900

    [Bronze I] Title: 성 지키기, Time: 120 ms, Memory: 21056 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.kt
+++ 백준/Bronze/1236. 성 지키기/성 지키기.kt
@@ -12,37 +12,62 @@ fun main() {
         doubleArray[it] = br.readLine()!!.toCharArray()
     }
 
+    val existRow = BooleanArray(n)
+    val existColumn = BooleanArray(m)
+
     // 1. 각 행/열에 대해 경비원이 있는지 확인한다.
-    var existRowCount = 0
     for(i in 0 until n) {
-        var exist = false
         for(j in 0 until m) {
             if (doubleArray[i][j] == 'X') {
-                exist = true
-                break
+                existRow[i] = true
+                existColumn[j] = true
             }
         }
-        if(exist) existRowCount++
-    }
-
-    var existColumnCount = 0
-    for(j in 0 until m) {
-        var exist = false
-        for(i in 0 until n) {
-            if (doubleArray[i][j] == 'X') {
-                exist = true
-                break
-            }
-        }
-        if (exist) existColumnCount++
     }
 
     // 2. 보호받지 못한 행/열 개수를 구한다.
-    val needRowCount = n - existRowCount
-    val needColumnCount = m - existColumnCount
+    val existRowCnt = existRow.count { it == true }
+    val existColumnCnt = existColumn.count { it == true }
 
     // 3. 둘 중 큰 값을 출력한다.
-    bw.write(if (needRowCount > needColumnCount) needRowCount.toString() else needColumnCount.toString())
+    val needRowCnt = n - existRowCnt
+    val needColumnCnt = m - existColumnCnt
+    bw.write(if (needRowCnt > needColumnCnt) needRowCnt.toString() else needColumnCnt.toString())
+
+    // 전체 시간 복잡도: O(N * M) 입력 + O(N * M) + O(N) + O(M) => (ONM)
+
+//    // 1. 각 행/열에 대해 경비원이 있는지 확인한다.
+//    var existRowCount = 0
+//    for(i in 0 until n) {
+//        var exist = false
+//        for(j in 0 until m) {
+//            if (doubleArray[i][j] == 'X') {
+//                exist = true
+//                break
+//            }
+//        }
+//        if(exist) existRowCount++
+//    }
+//
+//    var existColumnCount = 0
+//    for(j in 0 until m) {
+//        var exist = false
+//        for(i in 0 until n) {
+//            if (doubleArray[i][j] == 'X') {
+//                exist = true
+//                break
+//            }
+//        }
+//        if (exist) existColumnCount++
+//    }
+//
+//    // 2. 보호받지 못한 행/열 개수를 구한다.
+//    val needRowCount = n - existRowCount
+//    val needColumnCount = m - existColumnCount
+//
+//    // 3. 둘 중 큰 값을 출력한다.
+//    bw.write(if (needRowCount > needColumnCount) needRowCount.toString() else needColumnCount.toString())
+
     bw.flush()
     bw.close()
 }
