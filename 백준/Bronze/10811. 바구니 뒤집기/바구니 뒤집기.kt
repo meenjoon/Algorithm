96d63ada421465841e5fda57bf6835631d6dffaf
meenjoon <88024665+meenjoon@users.noreply.github.com>
2025-04-06 15:38:19 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 128 ms, Memory: 21552 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.kt
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.kt
@@ -6,25 +6,41 @@ fun main() {
 
     val (n, m) = br.readLine()!!.trim().split(" ").map { it.toInt() }
 
-    val array = IntArray(n) { it + 1 }
+    /**
+     * 배열의 반복문을 통해 정직하게 구현
+     */
+//    val array = IntArray(n) { it + 1 }
+//
+//    repeat(m) {
+//        val (a, b) = br.readLine()!!.trim().split(" ").map { it.toInt() }
+//
+//        var start = a - 1
+//        var end = b - 1
+//
+//        while(start < end) {
+//            val temp = array[start]
+//            array[start] = array[end]
+//            array[end] = temp
+//
+//            start++
+//            end--
+//        }
+//    }
+//
+//    bw.write(array.joinToString(" "))
+
+    /**
+     * List의 subList() 함수 활용 방법
+     */
+    val list = MutableList(n) { it + 1 }
 
     repeat(m) {
         val (a, b) = br.readLine()!!.trim().split(" ").map { it.toInt() }
 
-        var start = a - 1
-        var end = b - 1
-
-        while(start < end) {
-            val temp = array[start]
-            array[start] = array[end]
-            array[end] = temp
-
-            start++
-            end--
-        }
+        list.subList(a - 1, b).reverse()
     }
 
-    bw.write(array.joinToString(" "))
+    bw.write(list.joinToString(" "))
     bw.flush()
     bw.close()
 }
