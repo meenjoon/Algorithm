463aa37f948c023fd9ed45f0b30020d4dc9b8993
meenjoon <88024665+meenjoon@users.noreply.github.com>
2025-04-06 12:26:01 +0900

    [Silver III] Title: 두 수의 합, Time: 272 ms, Memory: 37904 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.kt
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.kt
@@ -1,21 +1,34 @@
-private var n  = 0
-private lateinit var arr : IntArray
-private var x  = 0
-private lateinit var subs : BooleanArray
+import java.io.*
 
 fun main() {
-    val br = System.`in`.bufferedReader()
-    n = br.readLine().toInt()
-    arr = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-    x = br.readLine().toInt()
-    subs = BooleanArray(x+1){false}
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
 
-    var result = 0
-    for(i in 0 until n){
-        if(arr[i]>=x) continue
-        if(subs[x-arr[i]])
-            result += 1
-        subs[arr[i]] = true
+    val n = br.readLine()!!.toInt()
+
+    val array = br.readLine()!!.trim().split(" ").map {
+        it.toInt()
+    }.toTypedArray()
+
+    val x = br.readLine()!!.toInt()
+
+    // input으로 들어온 수에 대해, 해당하는 인덱스에 true로 할당하는 BooelanArray 정의
+    val exist = BooleanArray(1000001).also { booleanArray ->
+        array.forEach {
+            booleanArray[it] = true
+        }
     }
-    println(result)
+
+    var cnt = 0
+    for(i in 0 until n) {
+        val pairValue = x - array[i]
+        if(0 <= pairValue && pairValue <= 1000000) {
+            // x가 8일 때 가정을 하고, 동일한 수(4+4)와 같은 경우에도 cnt++를 해도 괜찮은 이유가 어차피 나누기 2를 하면 x가 짝수든 홀수든 처리가 가능하기 때문
+            cnt += if(exist[pairValue]) 1 else 0
+        }
+    }
+
+    bw.write("${cnt/2}")
+    bw.flush()
+    bw.close()
 }
